24dca99f8a5b4b244e0bafe54bdb0033616a096b
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-06-17 21:31:06 -0700

    wins and draws work, still need button for new game

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var app = angular.module("TicApp", []);
 
 app.controller("TicCtrl", ["$scope", 
   function($scope) {
+    
     $scope.squares = ["","","","","","","","",""];
+    $scope.isPlayed = false;
+    $scope.winner = false;
+    $scope.draw = false;
+    
     var winningArrays = [
       [0,1,2], [0,3,6], [0,4,8],
       [1,4,7], [2,5,8], [3,4,5],
@@ -19,41 +24,32 @@ app.controller("TicCtrl", ["$scope",
     playerTwo.squares = [];
 
     var turnCount = 1;
-    $scope.currentPlayer = playerOne;
-    $scope.isPlayed = false;
-    $scope.winner = false;
 
     $scope.move = function() {
-      if (turnCount < 10 && this.isPlayed == false && turnCount % 2 !== 0) {
+      if (turnCount < 10 && this.isPlayed == false) {
+        if (turnCount % 2 !== 0) {
+          $scope.currentPlayer = playerOne;
+        } else {
+          $scope.currentPlayer = playerTwo;
+        }
         turnCount ++;
         this.isPlayed = true;
         this.square = $scope.currentPlayer.value;
         $scope.currentPlayer.squares.push(this.$index);
         checkForWin();
-        $scope.currentPlayer = playerTwo;
-      } else if (turnCount < 9 && this.isPlayed == false && turnCount % 2 === 0) {
-        turnCount ++;
-        this.isPlayed = true;
-        this.square = $scope.currentPlayer.value;
-        $scope.currentPlayer.squares.push(this.$index);
-        checkForWin();
-        $scope.currentPlayer = playerOne;
       } else {
         console.log("Illegal Move");
-        console.log(this);
-        console.log(this.isPlayed);
       }
     };
 
     var checkForWin = function() {
-      console.log($scope.currentPlayer.squares)
       for (var i = 0; i < winningArrays.length; i++) {
         if (_.intersection($scope.currentPlayer.squares, winningArrays[i]).length === 3) {
           $scope.winner = true;
-          console.log($scope.currentPlayer);
-          console.log($scope.squares)
-          this.isPlayed = true;
         }
+      }
+      if (turnCount === 10 && $scope.winner == false) {
+        $scope.draw = true;
       }
     };
   }
